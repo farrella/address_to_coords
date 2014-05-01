52c1f65c0438b20b6a68a42ecb3cc3b963b23520
Abby Farrell <abbykass@gmail.com>
2014-05-01 12:14:33 -0500

    API Phase 1 - everything except dynamic url

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,11 +3,12 @@ require 'json'
 
 class AddressesController < ApplicationController
   def fetch_coordinates
-    @address = "the corner of Foster and Sheridan"
+    @address = "the corner of Southport and Diversey"
     @url_safe_address = URI.encode(@address)
 
     # Your code goes here.
-    url = "http://maps.googleapis.com/maps/api/geocode/json?address=the+corner+of+Foster+and+Sheridan&sensor=false"
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=the+corner+of+Southport+and+Diversey&sensor=false"
+    # ("http://maps.googleapis.com/maps/api/geocode/json?address="+@url_safe_addresss.to_s)
     raw_data = open(url).read
     parsed_data = JSON.parse(raw_data)
     results = parsed_data["results"]
